100a3e3e84b161841af97f122ddb3948bc86ee4c
Eric Stensland <estensland88@gmail.com>
2015-02-22 08:30:30 -0600

    working on new route

--- app/scripts/dictionary.js
+++ app/scripts/dictionary.js
@@ -14,13 +14,41 @@ angular.module('myApp.dictionary', ['ngRoute'])
         }]
       }
     });
+    $routeProvider.when('/dictionary/:root', {
+      templateUrl: 'partials/dictionary/show.html',
+      controller: 'EntryCtrl',
+      resolve: {
+        json_grab: ['$http','$routeParams', function($http, $routeParams) {
+          return $http.get('/api/roots.json').then(function(response) {
+            return response.data;
+          });
+        }]
+      }
+    });
   }])
 
   .controller('DictionaryCtrl', ['$scope', '$routeParams', 'json_grab', function($scope, $routeParams, json_grab) {
     $scope.title = 'Dictionary';
     $scope.codex = json_grab.codex;
     $scope.roots = json_grab.roots;
-    $scope.propogateRoot = function(){
-      console.log('test')
+}])
+
+  .controller('EntryCtrl', ['$scope', '$routeParams', 'json_grab', function($scope, $routeParams, json_grab) {
+    $scope.title = 'Dictionary: ' + $routeParams;
+    $scope.codex = json_grab.codex;
+    $scope.letters = $routeParams
+    $scope.root = json_grab.roots[$routeParams];
+
+    var rootRecord = $scope.roots[root];
+    var wings = [];
+
+    for(var i = 0, ii =  rootRecord.sequence.length; i < ii; i ++){
+      var codex = $scope.codex[rootRecord.sequence[i]];
+
+      var feather = codex.replace(/\$/, root[0]);
+      feather = feather.replace(/\^/, root[1]);
+      feather = feather.replace(/\*/, root[2]);
+      wings.push({feather: rootRecord.sequence[i].def});
     }
+    $scope.root.wings = wings;
 }])
